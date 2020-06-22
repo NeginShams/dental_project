d95343493970de258263c342304eeedeac7ba27b
NeginShams <neginshams20@gmail.com>
2020-06-22 09:07:02 +0430

    test

--- dental_project/seapp/tests.py
+++ dental_project/seapp/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.utils import timezone
+import datetime
+from .models import Visit
+
+
+class QuestionModelTests(TestCase):
+
+    def test_was_reserved(self):
+
+        future_question = Visit(time = timezone.now() + datetime.timedelta(days=30))
+        self.assertIs(future_question.reserved, True)
 
 # Create your tests here.
+
